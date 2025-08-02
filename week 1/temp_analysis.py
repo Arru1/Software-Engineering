e0bd0e9612b7e2a5893671919c173ab078069d80
Arru1 <103086686+Arru1@users.noreply.github.com>
2025-08-03 09:57:23 +1200

    Add files via upload

---
+++ week 1/temp_analysis.py
@@ -0,0 +1,21 @@
+# Week1- Activity 3: Temperature Analysis Using Python and NumPy
+# Develop a Python project to perform the following tasks:
+# Calculate and print the average temperature for the week using NumPy.
+# Temperature data (in °C): [18.5, 19, 20, 25.0, 2, 30, 13.9]
+# Determine and display the highest and lowest recorded temperatures.
+# Convert all temperatures to Fahrenheit and print the converted values.
+# (Use the formula: F = C × 9/5 + 32)
+# Identify and print the indices of days where the temperature exceeded 20°C.
+import numpy as np
+temp=np.array([18.5, 19, 20, 25.0, 2, 30, 13.9])
+max_value=np.max(temp)
+min_value=np.min(temp)  
+print("Highest number:",max_value)  
+print("Lowest number:", min_value)
+
+celsius_temp=np.array([18.5, 19, 20, 25.0, 2, 30, 13.9])
+fahrenheit_temp=celsius_temp*9/5+32
+print("Temperature in fahrenheit:",fahrenheit_temp)
+
+hotdays_indices=np.where(celsius_temp>20)
+print("Indices of days with temperature more than 20°C:", hotdays_indices[0])
